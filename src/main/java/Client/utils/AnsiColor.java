525d8c0e2a77448c5261455d1bacf507cea16bbd
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-12 22:31:25 +0330

    feat: add AnsiColor

---
+++ src/main/java/Client/utils/AnsiColor.java
@@ -0,0 +1,11 @@
+package Client.utils;
+
+public class AnsiColor {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String WHITE = "\u001B[97m";
+    public static final String GRAY = "\u001B[90m";
+    public static final String CYAN = "\u001B[96m";
+    public static final String BORDER = "\u001B[94m";
+}
